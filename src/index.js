bf529b90ff4fb7b54703698d8b79f966c173f28a
Abhay999 <abhayjaat999@gmail.com>
2022-09-13 17:28:09 +0530

    1St

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://huddamanish01:ft7wnnSOT38Uv10u@cluster0.3zwwutm.mongodb.net/Project_2", {
+    useNewUrlParser: true
+})
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
