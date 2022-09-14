f2a0b933e1bfebbedc0cf0d1f844a962384c9323
Manpreet110 <Manpreetkaur44291@gmail.com>
2022-09-14 16:43:48 +0530

    updated

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -17,13 +17,24 @@ const createCollege = async function (req, res) {
         if (!fullName) return res.status(400).send({ status: false, msg: "Fullname is required" })
         if (!logoLink) return res.status(400).send({ status: false, msg: "logoLink is required" })
 
-        if (typeof (name) !== "string") return res.status(400).send({ status: false, msg: "Name must be string" })
-        if (typeof (fullName) !== "string") return res.status(400).send({ status: false, msg: "fullName must be string" })
+        // if (typeof (name) !== "string") return res.status(400).send({ status: false, msg: "Name must be string" })
+        // if (typeof (fullName) !== "string") return res.status(400).send({ status: false, msg: "fullName must be string" })
+        if (!validfun.nameValidation(name)) {
+            return res.status(400).send({ status: false, msg: "Please enter name" })}
+
+            if (!validfun.nameValidation(fullName)) {
+                return res.status(400).send({ status: false, msg: "Please enter full name!!" })}
+
         if (typeof (logoLink) !== "string") return res.status(400).send({ status: false, msg: "logoLink must be string" })//this is for url
 
         if (isDeleted) {
             if (typeof (isDeleted) !== "boolean")
                 return res.status(400).send({ status: false, msg: "isDeleted must be a Boolean value" })
+        }
+
+        if (isDeleted){
+            if (isDeleted===true)
+                return res.status(400).send({ status: false, msg: "Provide Is deleted key must be false" })
         }
         
         if (!urlPattern.test(logoLink)) {return res.status(400).send({ status: false, msg: "Invalid url" })}
